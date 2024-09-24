ac72c6ea5e2ccd0f587d3394c38232ae0d6f7141
george-coast <george.taylor@coastautonomous.com>
2024-09-24 17:29:29 -0400

    Update service.go

--- pkg/service.go
+++ pkg/service.go
@@ -17,74 +17,103 @@ type JumpCloudConnector interface {
 
 // RunService is the main entry point for the service. It will run a single time and return an error if one is encountered.
 func RunService(timeTracker TimeTracker, j JumpCloudConnector, pathToLogFile string) error {
-	f, err := os.OpenFile(pathToLogFile,
-		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
+    f, err := os.OpenFile(pathToLogFile,
+        os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+    defer f.Close()
+    lastTime := timeTracker.GetLastTime()
+    e, err := j.GetEventsSinceTime(lastTime)
+    if err != nil {
+        return err
+    }
+    // Before doing anything make sure there is at least one event
+    if len(e.Directory) == 0 && len(e.LDAP) == 0 && len(e.Systems) == 0 && len(e.SSO) == 0 && len(e.Radius) == 0 && len(e.PasswordManager) == 0 {
+        return nil
+    }
+    lastEventSeen := lastTime
 
-	lastTime := timeTracker.GetLastTime()
-	e, err := j.GetEventsSinceTime(lastTime)
-	if err != nil {
-		return err
-	}
+    // Process Directory events
+    for _, x := range e.Directory {
+        timestamp, err := time.Parse(time.RFC3339, x.Timestamp)
+        if err != nil {
+            fmt.Printf("Error parsing directory timestamp: %v\n", err)
+            continue
+        }
+        if timestamp.After(lastEventSeen) {
+            lastEventSeen = timestamp
+        }
+        _, writeErr := f.WriteString(x.convertToWazuhString() + "\n")
+        if writeErr != nil {
+            fmt.Printf("Error writing to file: %s", writeErr.Error())
+        }
+    }
 
-	// Before doing anything make sure there is at least one event, if there isn't we don't need to do anything
-	if len(e.Directory) == 0 && len(e.LDAP) == 0 && len(e.Systems) == 0 && len(e.SSO) == 0 && len(e.Radius) == 0 {
-		return nil
-	}
+    // Process LDAP events
+    for _, x := range e.LDAP {
+        timestamp, err := time.Parse(time.RFC3339, x.Timestamp)
+        if err != nil {
+            fmt.Printf("Error parsing LDAP timestamp: %v\n", err)
+            continue
+        }
+        if timestamp.After(lastEventSeen) {
+            lastEventSeen = timestamp
+        }
+        _, writeErr := f.WriteString(x.convertToWazuhString() + "\n")
+        if writeErr != nil {
+            fmt.Printf("Error writing to file: %s", writeErr.Error())
+        }
+    }
 
-	lastEventSeen := lastTime
+    // Process Systems events
+    for _, x := range e.Systems {
+        timestamp, err := time.Parse(time.RFC3339, x.Timestamp)
+        if err != nil {
+            fmt.Printf("Error parsing systems timestamp: %v\n", err)
+            continue
+        }
+        if timestamp.After(lastEventSeen) {
+            lastEventSeen = timestamp
+        }
+        _, writeErr := f.WriteString(x.convertToWazuhString() + "\n")
+        if writeErr != nil {
+            fmt.Printf("Error writing to file: %s", writeErr.Error())
+        }
+    }
 
-	// Loop over all events and find the newest timestamp, we will use this to update the last time we ran the service
-	for _, x := range e.Directory {
-		if x.Timestamp.After(lastEventSeen) {
-			lastEventSeen = x.Timestamp
-		}
-		_, writeErr := f.WriteString(x.convertToWazuhString() + "\n")
-		if writeErr != nil {
-			fmt.Printf("Error writing to file: %s", writeErr.Error())
-		}
-	}
-	for _, x := range e.LDAP {
-		if x.Timestamp.After(lastEventSeen) {
-			lastEventSeen = x.Timestamp
-		}
-		_, writeErr := f.WriteString(x.convertToWazuhString() + "\n")
-		if writeErr != nil {
-			fmt.Printf("Error writing to file: %s", writeErr.Error())
-		}
-	}
-	for _, x := range e.Systems {
-		if x.Timestamp.After(lastEventSeen) {
-			lastEventSeen = x.Timestamp
-		}
-		_, writeErr := f.WriteString(x.convertToWazuhString() + "\n")
-		if writeErr != nil {
-			fmt.Printf("Error writing to file: %s", writeErr.Error())
-		}
-	}
-	for _, x := range e.SSO {
-		if x.Timestamp.After(lastEventSeen) {
-			lastEventSeen = x.Timestamp
-		}
-		_, writeErr := f.WriteString(x.convertToWazuhString() + "\n")
-		if writeErr != nil {
-			fmt.Printf("Error writing to file: %s", writeErr.Error())
-		}
-	} // Closing brace for SSO loop
+    // Process SSO events
+    for _, x := range e.SSO {
+        timestamp, err := time.Parse(time.RFC3339, x.Timestamp)
+        if err != nil {
+            fmt.Printf("Error parsing SSO timestamp: %v\n", err)
+            continue
+        }
+        if timestamp.After(lastEventSeen) {
+            lastEventSeen = timestamp
+        }
+        _, writeErr := f.WriteString(x.convertToWazuhString() + "\n")
+        if writeErr != nil {
+            fmt.Printf("Error writing to file: %s", writeErr.Error())
+        }
+    }
 
-	for _, x := range e.PasswordManager {
-		if x.Timestamp.After(lastEventSeen) {
-			lastEventSeen = x.Timestamp
-		}
-		_, writeErr := f.WriteString(x.convertToWazuhString() + "\n")
-		if writeErr != nil {
-			fmt.Printf("Error writing to file: %s", writeErr.Error())
-		}
-	}
+    // Process Password Manager events
+    for _, x := range e.PasswordManager {
+        timestamp, err := time.Parse(time.RFC3339, x.Timestamp)
+        if err != nil {
+            fmt.Printf("Error parsing password manager timestamp: %v\n", err)
+            continue
+        }
+        if timestamp.After(lastEventSeen) {
+            lastEventSeen = timestamp
+        }
+        _, writeErr := f.WriteString(x.convertToWazuhString() + "\n")
+        if writeErr != nil {
+            fmt.Printf("Error writing to file: %s", writeErr.Error())
+        }
+    }
 
-	err = timeTracker.UpdateLast(lastEventSeen.Add(time.Second * 1))
-	return err
+    err = timeTracker.UpdateLast(lastEventSeen.Add(time.Second * 1))
+    return err
 }
